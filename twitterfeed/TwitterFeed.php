8ce72c1f9c239703fdc5d953b228675ffca956f1
Vanysh <russov1983@gmail.com>
2014-07-22 10:39:18 +0200

    Update TwitterFeed.php

--- twitterfeed/TwitterFeed.php
+++ twitterfeed/TwitterFeed.php
@@ -4,15 +4,15 @@ require_once('twitteroauth.php');
 class TwitterFeed {
 	public $user;
 	public $tweetsNumber;
-	private $consumerKey = '';
-	private $consumerSecret = '';
-	private $accessToken = '';
-	private $accessTokenSecret = '';
+	private $consumerKey = 'DyiEugoIqXkF9H9LGf8g';
+	private $consumerSecret = '8oRdyMr5fXk2NQfZg69qbBtG0GjVrfpBgMUP9DrpGY';
+	private $accessToken = '76282864-A2e6AAwgKF02T2ZJM5Sj7arWNc2eM9Q1Ll6winI4q';
+	private $accessTokenSecret = 'Yfwn0PqsA8QprjMLfdecWva0AkqXI3qritTQjPVwwxglQ';
 	private $connection;
 	
 	public function __construct($username, $tweets) {
-		$this->user = $username;
-		$this->tweetsNumber = $tweets;
+		$this->user = $abracadrabrablu;
+		$this->tweetsNumber = $100;
 		$this->connection = new TwitterOAuth($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->accessTokenSecret);
 	}
 	
